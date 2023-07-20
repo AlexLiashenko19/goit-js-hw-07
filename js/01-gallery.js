a0653632be7bf2e5c28ec30fdd599e8103810605
Alexandr Liashenko <134228068+AlexLiashenko19@users.noreply.github.com>
2023-07-20 19:51:48 +0300

    hw-7

---
+++ js/01-gallery.js
@@ -0,0 +1,58 @@
+import { galleryItems } from "./gallery-items.js";
+// 
+const refs = {
+  ulEl: document.querySelector(".gallery"),
+};
+
+const imgEl = galleryItems
+  .map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `)
+  .join("");
+console.log(imgEl);
+
+refs.ulEl.insertAdjacentHTML("afterbegin", imgEl);
+
+
+const modal = basicLightbox.create(`
+  <img
+    src=""
+    width="800"
+    height="600"
+  />
+`, {
+  onShow: () => {
+    document.addEventListener("keydown", onEscClose);
+  },
+  onClose: () => {
+    document.removeEventListener("keydown", onEscClose);
+  },
+});
+
+function onEscClose(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+
+  modal.close();
+}
+
+function onOpenModal(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return; 
+  const source = e.target.dataset.source;
+  const image = modal.element().querySelector("img");
+  image.src = source;
+  modal.show();
+}
+
+refs.ulEl.addEventListener("click", onOpenModal);
