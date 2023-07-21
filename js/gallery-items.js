29dd0c7fa476cdecc440073fe2a0d7dc4c82db67
Alexandr Liashenko <134228068+AlexLiashenko19@users.noreply.github.com>
2023-07-21 15:00:57 +0300

    Update gallery-items.js

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -25,7 +25,7 @@ export const galleryItems = [
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Те',
+    description: 'Ти',
   },
   {
     preview:
